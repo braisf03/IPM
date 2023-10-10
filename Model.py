bb78b6f5b0c9bb53516c519bc6067e5f05ed74a7
guillen-porto <114753260+guillen-porto@users.noreply.github.com>
2023-10-10 20:33:13 +0200

    Código más a tope

--- Model.py
+++ Model.py
@@ -10,9 +10,104 @@ class Model:
                 response = requests.get(url)
 
                 if response.status_code == 200:
-                    data = response.json()
-                    return data
+                    return response
                     
+
+                else:
+                    return None
+    
+            except requests.exceptions.RequestException as e:
+                return None
+        
+        def searchById(self, id: int):
+
+            try:
+                url = f"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={id}"  
+                response = requests.get(url)
+
+                if response.status_code == 200:
+                    return response
+                    
+
+                else:
+                    return None
+    
+            except requests.exceptions.RequestException as e:
+                return None
+
+
+        def searchByName(self, name: str):
+
+            try:
+                url = f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={name}"  
+                response = requests.get(url)
+
+                if response.status_code == 200:
+                    return response
+                    
+
+                else:
+                    return None
+    
+            except requests.exceptions.RequestException as e:
+                return None
+            
+
+
+        def searchByFirstLetter(self, letter: chr):
+        
+
+            try:
+                url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f={letter}"  
+                response = requests.get(url)
+
+                if response.status_code == 200:
+                    return response
+                    
+
+                else:
+                    return None
+    
+            except requests.exceptions.RequestException as e:
+                return None    
+
+        def categoryFilter(self, isCocktail: bool):
+            resultado = None
+
+            try:
+                if(isCocktail):
+                    url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?c=Cocktail"  
+                    response = requests.get(url)
+
+                else:
+                    url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?c=Ordinary_Drink"
+                    response = requests.get(url)
+
+                if response.status_code == 200:
+                    return response   
+
+                else:
+                    return None
+    
+            except requests.exceptions.RequestException as e:
+                print(f"Error de solicitud: { str(e)}")
+                return None 
+
+        def alcoholFilter(self, hasAlcohol: bool):
+            resultado = None
+
+            try:
+                if(hasAlcohol):
+                    url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?a=Alcoholic"  
+                    response = requests.get(url)
+
+                else:
+                    url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?a=Non_Alcoholic"
+                    response = requests.get(url)
+
+
+                if response.status_code == 200:
+                    return response                    
 
                 else:
                     print(f"Error en la solicitud: {response.status_code}")
@@ -20,13 +115,28 @@ class Model:
     
             except requests.exceptions.RequestException as e:
                 print(f"Error de solicitud: { str(e)}")
-                return None
+                return None    
 
+        
+        def glassFilter(isCocktailGlass):
+            resultado = None
 
+            try:
+                if(isCocktailGlass):
+                    url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?g=Cocktail_Glass"  
+                    response = requests.get(url)
 
-# Create an instance of the Modelo class
-modelo = Model()
+                else:
+                    url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?g=Champagne_Flute"
+                    response = requests.get(url)
 
-# Call the get_data method with some data (you should replace 'data' with the actual data you want to use)
-datos = modelo.actualizar_Api()
-print(f"{datos}")
+                if response.status_code == 200:
+                    return response
+                    
+                else:
+                    return None
+    
+            except requests.exceptions.RequestException as e:
+                return None 
+ç
+                
