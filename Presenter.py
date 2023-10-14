6923abe4fb4034726ec88f2757f3b2584cfbfdbc
Brais <114221451+braisf03@users.noreply.github.com>
2023-10-14 18:51:44 +0200

    Update Presenter.py

--- Presenter.py
+++ Presenter.py
@@ -1,41 +1,40 @@
 from Model import Model
 import json
+import random
 
 # Funcion auxiliar que usamos para que no se repita tanto código que parsea el JSON
 
-def parseUrlDetalle(response):
-    results = []
+class Presenter:
 
-    json_data = json.loads(response)
+    def parseUrlDetalle(response):
+        results = []
 
-    json_data1 = json_data["drinks"][0]
+        json_data = json.loads(response)
 
-    #Esto recorre el json guardando los cocktails en una lista
-    for i in range(0,len(json_data["drinks"])):
-        json_data1 = json_data["drinks"][i]
-        results.append(json_data1)
+        #Esto recorre el json guardando los cocktails en una lista
+        for i in range(0,len(json_data["drinks"])):
+            json_data1 = json_data["drinks"][i]
+            results.append(json_data1)
 
-    #Devuelve el JSON como string
-    return results
+        #Devuelve el JSON como string
+        return results
 
-def parseUrlNoDetalle(response):
-    results = []
+    def parseUrlNoDetalle(response):
+        results = []
 
-    json_data = json.loads(response)
+        json_data = json.loads(response)
 
-    json_data1 = json_data["drinks"][0]
-
-    #Esto recorre el json guardando los cocktails en una lista
-    for i in range(0,len(json_data["drinks"])):
+        #Esto recorre el json guardando los cocktails en una lista
+        for i in range(0,len(json_data["drinks"])):
             json_data1 = json_data["drinks"][i]
             results.append(json_data1["strDrink"] + " " + json_data1["strDrinkThumb"])
 
-    #Devuelve el JSON como string
-    return results
+        #Devuelve el JSON como string
+        return results
 
-# Funcion para los ingredientes
+    # Funcion para los ingredientes
 
-def parseUrlIng(response):
+    def parseUrlIng(response):
         results = []
 
         json_data = json.loads(response)
@@ -50,173 +49,263 @@ def parseUrlIng(response):
         #Devuelve el JSON como string
         return results
 
-# FUNCIONES DE BUSCAR COCKTAILS
-# Funciones que usamos para buscar por Nombre
+    # FUNCIONES DE BUSCAR COCKTAILS
+    # Funciones que usamos para buscar por Nombre
 
-def cocktailNoDetalleName(identificador):
-    try:
-        # Recibo el JSON de la capa modelo y lo formateo
-        response = Model.searchByName(identificador).text
-        results = parseUrlNoDetalle(response)
+    def cocktailNoDetalleName(identificador):
+        try:
+            # Recibo el JSON de la capa modelo y lo formateo
+            response = Model.searchByName(identificador).text
+            results = Presenter.parseUrlNoDetalle(response)
 
-        #Devolvemos la lista de JSON
-        #print(results)
-        return results
-    except Exception as e:
-        print(f"Error: {e}")
+            #Devolvemos la lista de JSON
+            print(results)
+            return results
+        except Exception as e:
+            print(f"Error: {e}")
 
-def cocktailDetalleName(identificador):
-    try:
-        # Recibo el JSON de la capa modelo y lo formateo
-        response = Model.searchByName(identificador).text
-        results = parseUrlDetalle(response)
+    def cocktailDetalleName(identificador):
+        try:
+            # Recibo el JSON de la capa modelo y lo formateo
+            response = Model.searchByName(identificador).text
+            results = Presenter.parseUrlDetalle(response)
 
-        #Devolvemos la lista de JSON
-        #print(results)
-        return results
-    except Exception as e:
-        print(f"Error: {e}")
+            #Devolvemos la lista de JSON
+            #print(results)
+            return results
+        except Exception as e:
+            print(f"Error: {e}")
 
-# Funciones que usamos para buscar por Id
+    # Funciones que usamos para buscar por Id
 
-def cocktailNoDetalleId(identificador):
-    try:
-        # Recibo el JSON de la capa modelo y lo formateo
-        response = Model.searchById(identificador).text
-        results = parseUrlNoDetalle(response)
+    def cocktailNoDetalleId(identificador):
+        try:
+            # Recibo el JSON de la capa modelo y lo formateo
+            response = Model.searchById(identificador).text
+            results = Presenter.parseUrlNoDetalle(response)
 
-        #Devolvemos la lista de JSON
-        #print(results)
-        return results
-    except Exception as e:
-        print(f"Error: {e}")
+            #Devolvemos la lista de JSON
+            #print(results)
+            return results
+        except Exception as e:
+            print(f"Error: {e}")
 
-def cocktailDetalleId(identificador):
-    try:
-        # Recibo el JSON de la capa modelo y lo formateo
-        response = Model.searchById(identificador).text
-        results = parseUrlDetalle(response)
+    def cocktailDetalleId(identificador):
+        try:
+            # Recibo el JSON de la capa modelo y lo formateo
+            response = Model.searchById(identificador).text
+            results = Presenter.parseUrlDetalle(response)
 
-        #Devolvemos la lista de JSON
-        #print(results)
-        return results
-    except Exception as e:
-        print(f"Error: {e}")
+            #Devolvemos la lista de JSON
+            #print(results)
+            return results
+        except Exception as e:
+            print(f"Error: {e}")
 
-# Funciones que usamos para buscar por primera letra
+    # Funciones que usamos para buscar por primera letra
 
-def cocktailNoDetalleFirstLetter(identificador):
-    try:
-        # Recibo el JSON de la capa modelo y lo formateo
-        response = Model.searchByFirstLetter(identificador).text
-        results = parseUrlNoDetalle(response)
+    def cocktailNoDetalleFirstLetter(identificador):
+        try:
+            # Recibo el JSON de la capa modelo y lo formateo
+            response = Model.searchByFirstLetter(identificador).text
+            results = Presenter.parseUrlNoDetalle(response)
 
-        #Devolvemos la lista de JSON
-        #print(results)
-        return results
-    except Exception as e:
-        print(f"Error: {e}")
+            #Devolvemos la lista de JSON
+            #print(results)
+            return results
+        except Exception as e:
+            print(f"Error: {e}")
 
-def cocktailDetalleFirstLetter(identificador):
-    try:
-        # Recibo el JSON de la capa modelo y lo formateo
-        response = Model.searchByFirstLetter(identificador).text
-        results = parseUrlDetalle(response)
+    def cocktailDetalleFirstLetter(identificador):
+        try:
+            # Recibo el JSON de la capa modelo y lo formateo
+            response = Model.searchByFirstLetter(identificador).text
+            results = Presenter.parseUrlDetalle(response)
 
-        #Devolvemos la lista de JSON
-        #print(results)
-        return results
-    except Exception as e:
-        print(f"Error: {e}")
+            #Devolvemos la lista de JSON
+            #print(results)
+            return results
+        except Exception as e:
+            print(f"Error: {e}")
+    ######################################################
+    # Funcion para buscar por ingrediente
 
-# Funcion para buscar por ingrediente
-
-def cocktailByIngredient(identificador):
-    try:
+    def cocktailByIngredient(identificador):
+        try:
         
-        response = Model.searchByIng(identificador).text
-        results = parseUrlDetalle(response)
+            response = Model.searchByIng(identificador).text
+            results = Presenter.parseUrlDetalle(response)
         
+            #Devuelve el JSON como string
+            return results
+        except Exception as e:
+            print(f"Error: {e}")
+
+    # FUNCIONES PARA BUSCAR INGREDIENTES
+
+    def ingredientByName(identificador):
+        try:
+            # Recibo el JSON de la capa modelo y lo formateo
+            response = Model.searchIngByName(identificador).text
+            results = Presenter.parseUrlIng(response)
+        
+            return results
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def ingredientById(identificador):
+        try:
+            # Recibo el JSON de la capa modelo y lo formateo
+            response = Model.searchIngByID(identificador).text
+            results = Presenter.parseUrlIng(response)
+            #Devuelve el JSON como string
+            return results
+        except Exception as e:
+            print(f"Error: {e}")
+
+    # FUNCION PARA DAR UN RANDOM
+
+    def randomCocktail():
+        try:
+            # Recibo el JSON de la capa modelo y lo formateo
+            response = Model.getRandom().text
+            results = Presenter.parseUrlDetalle(response)
+
+            #Devolvemos la lista de JSON
+            #print(results)
+            return results
+        except Exception as e:
+            print(f"Error: {e}")
+
+    # FILTROS DE BÚSQUEDA
+    # Filtro de alcohol
+
+    def filterByAlcohol(identificador):
+        try:
+            # Recibo el JSON de la capa modelo y lo formateo
+            response = Model.alcoholFilter(identificador).text
+            results = Presenter.parseUrlDetalle(response)
+
+            #Devolvemos la lista de JSON
+            #print(results)
+            return results
+        except Exception as e:
+            print(f"Error: {e}")
+
+    # Filtro de categoría
+
+    def filterByCategory(identificador):
+        try:
+            # Recibo el JSON de la capa modelo y lo formateo
+            response = Model.categoryFilter(identificador).text
+            results = Presenter.parseUrlDetalle(response)
+
+            #Devolvemos la lista de JSON
+            #print(results)
+            return results
+        except Exception as e:
+            print(f"Error: {e}")
+
+    # Filtro de Vaso
+
+    def filterByGlass(identificador):
+        try:
+            # Recibo el JSON de la capa modelo y lo formateo
+            response = Model.glassFilter(identificador).text
+            results = Presenter.parseUrlDetalle(response)
+
+            #Devolvemos la lista de JSON
+            #print(results)
+            return results
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+    # Hacer una funcion 4 cocktails
+    def XcocktailNoDetalleName(identificador):
+        try:
+            # Recibo el JSON de la capa modelo y lo formateo
+            response = Model.searchByName(identificador).text
+            results_name = []
+            results_photo = []
+
+            json_data = json.loads(response)
+
+            if len(json_data["drinks"])>4:
+                numeros = random.sample(range(0,len(json_data["drinks"])),4)
+                print(numeros)
+                for i in range(0,4):
+                    json_data1 = json_data["drinks"][numeros[i]]["strDrink"]
+                    json_data2 = json_data["drinks"][numeros[i]]["strDrinkThumb"]
+                    results_name.append(json_data1)
+                    results_photo.append(json_data2)
+            else:
+                #Esto recorre el json guardando los cocktails en una lista
+                for i in range(0,len(json_data["drinks"])):
+                    json_data1 = json_data["drinks"][i]["strDrink"]
+                    json_data2 = json_data["drinks"][i]["strDrinkThumb"]
+                    results_name.append(json_data1)
+                    results_photo.append(json_data2)
+
+            #Devuelve el JSON como string
+            print(results_name)
+            print(results_photo)
+            return results_name,results_photo
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+    def get4Random():
+        results_name = []
+        restults_photo = []
+
+        #Esto recorre el json guardando los cocktails en una lista
+        for i in range(0,3):
+            response = Model.getRandom()
+            json_data = json.loads(response)
+
+            json_data1 = json_data["drinks"][0]["strDrink"]
+            json_data2 = json_data["drinks"][0]["strDrinkThumb"]
+            results_name.append(json_data1)
+            restults_photo.append(json_data2)
+
         #Devuelve el JSON como string
-        return results
-    except Exception as e:
-        print(f"Error: {e}")
+        return results_name,restults_photo
+    
 
-# FUNCIONES PARA BUSCAR INGREDIENTES
 
-def ingredientByName(identificador):
-    try:
-        # Recibo el JSON de la capa modelo y lo formateo
-        response = Model.searchIngByName(identificador).text
-        results = parseUrlIng(response)
-        
-        return results
-    except Exception as e:
-        print(f"Error: {e}")
+    def random4Cocktail():
+        try:
+            results_name = []
+            results_photo = []
 
-def ingredientById(identificador):
-    try:
-        # Recibo el JSON de la capa modelo y lo formateo
-        response = Model.searchIngByID(identificador).text
-        results = parseUrlIng(response)
-        #Devuelve el JSON como string
-        return results
-    except Exception as e:
-        print(f"Error: {e}")
+            while len(results_name) < 4:
+                response = Model.getRandom().text
+                json_data = json.loads(response)
 
-# FUNCION PARA DAR UN RANDOM
+                json_data1 = json_data["drinks"][0]["strDrink"]
+                json_data2 = json_data["drinks"][0]["strDrinkThumb"]
+                if json_data1 not in results_name:
+                    results_name.append(json_data1)
+                    results_photo.append(json_data2)
 
-def randomCocktail():
-    try:
-        # Recibo el JSON de la capa modelo y lo formateo
-        response = Model.getRandom().text
-        results = parseUrlDetalle(response)
+            return results_name,results_photo
+        except Exception as e:
+            print(f"Error: {e}")
 
-        #Devolvemos la lista de JSON
-        #print(results)
-        return results
-    except Exception as e:
-        print(f"Error: {e}")
 
-# FILTROS DE BÚSQUEDA
-# Filtro de alcohol
+# PRUEBAS DE CÓDIGO
 
-def filterByAlcohol(identificador):
-    try:
-        # Recibo el JSON de la capa modelo y lo formateo
-        response = Model.alcoholFilter(identificador).text
-        results = parseUrlDetalle(response)
+url = "margarita"
 
-        #Devolvemos la lista de JSON
-        #print(results)
-        return results
-    except Exception as e:
-        print(f"Error: {e}")
+#numeros = random.sample(range(0,5),4)
+#print(numeros)
 
-# Filtro de categoría
+#(l1,l2) = Presenter.XcocktailNoDetalleName(url)
 
-def filterByCategory(identificador):
-    try:
-        # Recibo el JSON de la capa modelo y lo formateo
-        response = Model.categoryFilter(identificador).text
-        results = parseUrlDetalle(response)
+(l1,l2) = Presenter.random4Cocktail()
 
-        #Devolvemos la lista de JSON
-        #print(results)
-        return results
-    except Exception as e:
-        print(f"Error: {e}")
+print(l1)
+print(l2)
 
-# Filtro de Vaso
 
-def filterByGlass(identificador):
-    try:
-        # Recibo el JSON de la capa modelo y lo formateo
-        response = Model.glassFilter(identificador).text
-        results = parseUrlDetalle(response)
-
-        #Devolvemos la lista de JSON
-        #print(results)
-        return results
-    except Exception as e:
-        print(f"Error: {e}")
